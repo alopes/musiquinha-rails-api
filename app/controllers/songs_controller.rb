7476569f015f9e0393529d6f8ab8fcb6e892ffcd
António Lopes <antonio@alopes.co.uk>
2014-05-22 23:47:50 +0100

    working on a cross domain issue

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -61,6 +61,14 @@ class SongsController < ApplicationController
     end
   end
 
+  def options
+    set_headers
+    # this will send an empty request to the clien with 200 status code (OK, can proceed)
+    render :text => '', :content_type => 'text/plain'
+  end
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_song
@@ -70,5 +78,14 @@ class SongsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
       params.require(:song).permit(:title)
+    end
+
+    # Set CORS
+    def set_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Expose-Headers'] = 'Etag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*, x-requested-with, Content-Type, If-Modified-Since, If-None-Match'
+      headers['Access-Control-Max-Age'] = '86400'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :songs, defaults: {format: :json}
+  match '/songs' => 'songs#options', :constraints => {:method => 'OPTIONS'}, via: [:options]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
