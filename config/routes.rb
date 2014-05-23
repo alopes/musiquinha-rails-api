05334f344c4caeaecbd08d2ab3210a4797dc40c7
António Lopes <antonio@alopes.co.uk>
2014-05-23 01:03:44 +0100

    Fixing some cross-domain drama

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :songs, defaults: {format: :json}
   match '/songs' => 'songs#options', :constraints => {:method => 'OPTIONS'}, via: [:options]
+  # http://blog.shulhi.com/2013/10/24/cross-domain-request-with-rails-+-angularjs/
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
